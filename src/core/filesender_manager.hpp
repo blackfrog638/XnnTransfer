f3fc40575eb82cf14fc06ebecc719a14d8648544
blackfrog <34157174+blackfrog638@users.noreply.github.com>
2025-05-01 11:04:03 +0800

    feat: add name and ip to the broadcast to visualize the user list

--- src/core/filesender_manager.hpp
+++ src/core/filesender_manager.hpp
@@ -18,21 +18,22 @@ class FilesenderManager {
         BroadcastManager broadcast_manager;
         FilesenderManager(std::string name, std::string password,
                           const short port) :
-            name(std::move(name)),
+            name(name),
             password(std::move(password)), 
-            port(port) {}
+            port(port),
+            broadcast_manager(std::move(name)) {}
         
-        explicit FilesenderManager(const short port) : port(port) {}
+        explicit FilesenderManager(const short port) : 
+            port(port),
+            //!TODO: 这里的name和password应该是从配置文件中读取的
+            broadcast_manager(name) {}
 
         void run_broadcast(){
-            BroadcastManager bm;
-
-            std::thread sender_thread([&bm, this]() { bm.broadcast_sender(port); });
-            std::thread receiver_thread([&bm, this]() { bm.broadcast_receiver(port); });
-
-            //TODO: 得到链接之后结束广播
+            std::thread sender_thread([&]() { broadcast_manager.broadcast_sender(port); });
+            std::thread receiver_thread([&]() { broadcast_manager.broadcast_receiver(port); });
+            //!TODO: 得到链接之后结束广播
             std::cin.get();
-            bm.stop_flag = true;
+            broadcast_manager.stop_flag = true;
 
             sender_thread.join();
             receiver_thread.join();
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ const short PORT = 8888;
 
 int main(){
     std::cout<<"Starting broadcast sender and receiver..."<<std::endl;
-    FilesenderManager fm(PORT);
+    FilesenderManager fm("blackfrog638","123456",PORT);
     std::thread broadcast_thread([&fm]() { fm.run_broadcast(); });
     std::cout<<"Press Enter to stop broadcast..."<<std::endl;
     broadcast_thread.join();
